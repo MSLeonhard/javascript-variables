bb29184f26ca534ccfabb88e996b1daf8f9191b0
Mercy Baffour <mercy.baffour@gmail.com>
2021-02-01 14:57:21 -0500

    Step 4

--- js/basics-variables-1.js
+++ js/basics-variables-1.js
@@ -5,3 +5,9 @@ console.log(daysPerYear)
 const hoursPerDay = 24
 
 console.log(hoursPerDay)
+
+console.log(hoursPerDay * daysPerYear)
+
+const minutesPerHour = 60
+
+console.log(minutesPerHour * hoursPerDay)
